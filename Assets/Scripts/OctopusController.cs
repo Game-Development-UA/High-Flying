e3e69cc625ede559ed9b689be498fcd78373ec01
Catie Baker <bakerc2@email.arizona.edu>
2019-02-07 11:59:32 -0700

    Added Rigidbody
    Added Rigidbody and partial script to Octopus

---
+++ Assets/Scripts/OctopusController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctopusController : MonoBehaviour
+{
+    public float horizontal;
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+    }
+}
