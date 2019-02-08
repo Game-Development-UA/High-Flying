f8c9ff166f360898ca8a7ce49b1125d775cbeb02
Catie Baker <bakerc2@email.arizona.edu>
2019-02-08 14:16:36 -0700

    Right Bird Changes. Airplane Created
    Right Birds script created. Right Birds move right to left across camera view. Airplane created.

---
+++ Assets/Scripts/RightBirdMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightBirdMovement : MonoBehaviour
+{
+    public Transform bird;
+    public float speed;
+
+    private void Start()
+    {
+        speed = Random.Range(1f, 7f);
+    }
+
+    void Update()
+    {
+        bird.position += new Vector3(-Time.deltaTime * speed, 0f, 0f);
+    }
+}
