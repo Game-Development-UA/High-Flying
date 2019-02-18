b4d86c75a908cfd0d301efbc9b8c85794b16dcdc
Catie Baker <bakerc2@email.arizona.edu>
2019-02-17 22:42:53 -0700

    UI stuff
    UI stuff is confusing af. I'm dead. Made menu scene, made pause screen for main scene.

--- Assets/Scripts/ToggleButton.cs
+++ Assets/Scripts/ToggleButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour
+public class ToggleButton : MonoBehaviour
 {
     public GameObject menu;
 
