4e447dac27efb2f6226191182d3e1aa7cd3f2774
Catie Baker <bakerc2@email.arizona.edu>
2019-02-17 23:25:23 -0700

    Load from Main Menu to Game

---
+++ Assets/Scripts/LoadMainScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadMainScene : MonoBehaviour
+{
+    public void Load (string MainGame)
+    {
+        SceneManager.LoadScene(MainGame);
+    }
+}
