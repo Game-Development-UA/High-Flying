fcbc932d4455f83f3a3df9ae0017d2eebbd18e04
Catie Baker <bakerc2@email.arizona.edu>
2019-02-26 10:12:53 -0700

    Collect Balloon Script

---
+++ Assets/Scripts/CollectBalloon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectBalloon : MonoBehaviour
+
+{ 
+public GameObject balloon;
+
+
+
+    void OnTriggerEnter2D(Collision2D collider)
+    {
+    balloon = collider.gameObject.GetComponent<balloon>;
+        if (balloon != null)
+        {
+        Destroy (balloon);
+        }
+    }
+}
