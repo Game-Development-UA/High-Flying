c2b26233ae34a4effb5d74046e056369be9f2f43
Catie Baker <bakerc2@email.arizona.edu>
2019-03-10 13:02:22 -0700

    Script additions
    Damageplayer, balloonspawn

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.name == "Balloon")
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/SpawnBalloon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalloon : MonoBehaviour
+{
+    float timer;
+    public Transform prefab;
+    public float delay;
+    public int balloon;
+    public float yMax;
+
+    private void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > delay)
+        {
+            for (int i = 0; i < balloon; i++)
+            {
+                Instantiate(prefab, transform.position + new Vector3(i * 2.0F, Random.Range(0, yMax), 0), Quaternion.identity);
+            }
+
+            timer = 0;
+        }
+
+    }
+}
