05d5368792d57e21a86e60396e34a569b03d2a50
Catie Baker <bakerc2@email.arizona.edu>
2019-04-18 14:22:14 -0700

    Narrowed Wall Parameters
    Narrowed the boundaries. Changed balloon spawn locations.

---
+++ Assets/Scripts/UIScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScore : MonoBehaviour
+{
+    public int scoreH;
+    public int scoreB;
+    public Text heightScore;
+    public Text balloonCount;
+
+    private void Start()
+    {
+        scoreH = 0; 
+    }
+
+    void Update()
+    {
+        scoreH = scoreH + y;
+        SetScoreH();
+    }
+
+    void SetScoreH()
+    {
+        heightScore
+    }
+}
